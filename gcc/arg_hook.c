03a69202230082d9f0d2a1719c2746daecade013
kongjiadongyuan <zhaggbl@foxmail.com>
2022-03-24 11:48:38 +0800

    inline uuid cjson and sqlite3 support

--- gcc/arg_hook.c
+++ gcc/arg_hook.c
@@ -1,6 +1,6 @@
 #define _GNU_SOURCE
 
-#define ARG_HOOK_DEBUG
+// #define ARG_HOOK_DEBUG
 
 #include "config.h"
 #include "system.h"
@@ -21,7 +21,9 @@
 #include "options.h"
 
 #include "arg_hook.h"
+#include "cJSON.h"
 #include "sqlite3.h"
+#include "uuid4.h"
 #include "rev_options.h"
 // char *rev_opt_code[] = {...}
 #include "incbin.h"
@@ -33,7 +35,6 @@
 #include <limits.h>
 #include <string_view>
 #include <iostream>
-#include <uuid/uuid.h>
 #include <sys/time.h>
 
 #define CREATE_TABLE "CREATE TABLE IF NOT EXISTS t_commands(\
@@ -43,25 +44,15 @@
                         output TEXT, \
                         cmdline TEXT, \
                         arg_idx INTEGER, \
-                        opt_idx INTEGER, \
-                        opt_name TEXT, \
-                        warn_message TEXT, \
-                        arg TEXT, \
-                        orig_option_with_args_text TEXT, \
-                        canonical_option_0 TEXT, \
-                        canonical_option_1 TEXT, \
-                        canonical_option_2 TEXT, \
-                        canonical_option_3 TEXT, \
-                        canonical_option_num_elements INTEGER, \
-                        value TEXT, \
-                        errors TEXT)"
+                        decoded_argv JSON);"
 
 
 char output_resolved_path[PATH_MAX] = {0};
-char runtime_uuid[UUID_STR_LEN] = {0};
+char runtime_uuid[UUID4_LEN] = {0};
 char arg_string[0x1000] = {0};
 uint64_t runtime_timestamp;
 bool OPT_o_found = false;
+bool arg_hook_failed = false;
 
 
 void print_cl_decoded_option(struct cl_decoded_option *opt){
@@ -97,8 +88,73 @@ void observe_decoded_options(unsigned int count, struct cl_decoded_option *optio
     }
 }
 
-void insert_decoded_option(sqlite3 *db, struct cl_decoded_option *option){
-    ;
+char *decoded_option_serialize(struct cl_decoded_option *option){
+    cJSON *json = NULL;
+    json = cJSON_CreateObject();
+    cJSON_AddNumberToObject(json, "opt_index", option->opt_index);
+    cJSON_AddStringToObject(json, "opt_name", rev_opt_code[option->opt_index]);
+    cJSON_AddStringToObject(json, "warn_message", option->warn_message);
+    cJSON_AddStringToObject(json, "arg", option->arg);
+    cJSON_AddStringToObject(json, "orig_option_with_args_text", option->orig_option_with_args_text);
+    cJSON *canonical_option = NULL;
+    canonical_option = cJSON_CreateArray();
+    cJSON_AddItemToArray(canonical_option, cJSON_CreateString(option->canonical_option[0]));
+    cJSON_AddItemToArray(canonical_option, cJSON_CreateString(option->canonical_option[1]));
+    cJSON_AddItemToArray(canonical_option, cJSON_CreateString(option->canonical_option[2]));
+    cJSON_AddItemToArray(canonical_option, cJSON_CreateString(option->canonical_option[3]));
+    cJSON_AddItemToObject(json, "canonical_option", canonical_option);
+    cJSON_AddNumberToObject(json, "canonical_option_num_elements", option->canonical_option_num_elements);
+    cJSON_AddNumberToObject(json, "value", option->value);
+    cJSON_AddNumberToObject(json, "errors", option->errors);
+    char *json_str = cJSON_Print(json);
+    cJSON_Delete(json);
+    return json_str;
+}
+
+int insert_decoded_option(sqlite3 *db, struct cl_decoded_option *option, unsigned int _option_idx){
+    char *insert_cmd;
+    int size = 0x200;
+    insert_cmd = (char *)xmalloc(size);
+    memset(insert_cmd, 0, size);
+    char *serialized_option = decoded_option_serialize(option);
+#ifdef ARG_HOOK_DEBUG
+    std::cerr << serialized_option << std::endl;
+#endif
+    while(true){
+        int writed = snprintf(
+            insert_cmd,
+            size,
+            "INSERT INTO t_commands VALUES(\n\
+                NULL,\n\
+                '%s',\n\
+                %ld,\n\
+                '%s',\n\
+                '%s',\n\
+                %d,\n\
+                '%s');",
+            runtime_uuid,
+            runtime_timestamp,
+            output_resolved_path,
+            arg_string,
+            _option_idx,
+            serialized_option
+        );
+        if (writed >= size - 1){
+            size = size * 2;
+            insert_cmd = (char *)xrealloc(insert_cmd, size);
+            memset(insert_cmd, 0, size);
+        }
+        else{
+            free(serialized_option);
+            break;
+        }
+    }
+#ifdef ARG_HOOK_DEBUG
+    std::cerr << insert_cmd << std::endl;
+#endif
+    int rc = sqlite3_exec(db, insert_cmd, NULL, NULL, NULL);
+    free(insert_cmd);
+    return rc;
 }
 
 void insert_decoded_options(unsigned int decoded_options_count, struct cl_decoded_option *decoded_options, int argc, char **argv){
@@ -106,9 +162,8 @@ void insert_decoded_options(unsigned int decoded_options_count, struct cl_decode
 
 
     // Generate UUID
-    uuid_t bin_uuid;
-    uuid_generate_random(bin_uuid);
-    uuid_unparse(bin_uuid, runtime_uuid);
+    uuid4_init();
+    uuid4_generate(runtime_uuid);
 
     
 #ifdef ARG_HOOK_DEBUG
@@ -128,6 +183,14 @@ void insert_decoded_options(unsigned int decoded_options_count, struct cl_decode
         std::cerr << "output_resolved_path: " << output_resolved_path << std::endl;
 #endif
     }
+    else
+    {
+#ifdef ARG_HOOK_DEBUG
+        std::cerr << "OPT_o not found" << std::endl;
+#endif
+        arg_hook_failed = true;
+        return;
+    }
 
     // Get timestamp
     struct timeval tv;
@@ -138,7 +201,6 @@ void insert_decoded_options(unsigned int decoded_options_count, struct cl_decode
 #endif
 
     // Restore arg_string
-    char arg_string[0x1000] = {0};
     for(int _arg_idx; _arg_idx < argc; _arg_idx++){
         strcat(arg_string, argv[_arg_idx]);
         if(_arg_idx < argc - 1)
@@ -156,6 +218,7 @@ void insert_decoded_options(unsigned int decoded_options_count, struct cl_decode
 #ifdef ARG_HOOK_DEBUG
         std::cerr << "COMPILE_COMMANDS_DB env not set" << std::endl;
 #endif
+        arg_hook_failed = true;
         return;
     }
     int rc = sqlite3_open(dbpath, &db);
@@ -163,18 +226,51 @@ void insert_decoded_options(unsigned int decoded_options_count, struct cl_decode
 #ifdef ARG_HOOK_DEBUG
         std::cerr << "database open failed" << std::endl;
 #endif
+        arg_hook_failed = true;
         return;
     }
 
-    
-
-    for(unsigned int _option_idx = 0; _option_idx < decoded_options_count; _option_idx ++){
-        insert_decoded_option(db, &decoded_options[_option_idx]);
+    // Create table
+    rc = sqlite3_exec(db, CREATE_TABLE, NULL, NULL, NULL);
+    if(rc != SQLITE_OK) {
+#ifdef ARG_HOOK_DEBUG
+        std::cerr << "create table failed" << std::endl;
+#endif
+        arg_hook_failed = true;
+        return;
     }
-    sqlite3_finalize(db);
+
+    // begin transaction
+    rc = sqlite3_exec(db, "begin;", NULL, NULL, NULL);
+    if(rc != SQLITE_OK) {
+#ifdef ARG_HOOK_DEBUG
+        std::cerr << "begin failed" << std::endl;
+#endif
+        arg_hook_failed = true;
+        return;
+    }
+
+    // insert each row
+    for(unsigned int _option_idx = 0; _option_idx < decoded_options_count; _option_idx ++){
+        rc = insert_decoded_option(db, &decoded_options[_option_idx], _option_idx);
+        if (rc != SQLITE_OK) {
+#ifdef ARG_HOOK_DEBUG
+            std::cerr << "insert failed" << std::endl;
+#endif
+            sqlite3_exec(db, "rollback;", NULL, NULL, NULL);
+            arg_hook_failed = true;
+            return;
+        }
+    }
+    sqlite3_exec(db, "commit;", NULL, NULL, NULL);
+    sqlite3_close(db);
 }
 
 void arg_hook_main(unsigned int decoded_options_count, struct cl_decoded_option *decoded_options, int argc, char **argv){
-    // observe_decoded_options(decoded_options_count, decoded_options);
+    // if COMPILE_COMMANDS_DB not set, return.
+    if (!getenv("COMPILE_COMMANDS_DB")){
+        return;
+    }
+    // Main logic start.
     insert_decoded_options(decoded_options_count, decoded_options, argc, argv);
 }
