bb7c3d6525d6fcff1469f752c371c6adb298e18e
kongjiadongyuan <zhaggbl@foxmail.com>
2022-04-19 19:20:33 +0800

    test

--- gcc/arg_hook.h
+++
@@ -1,8 +0,0 @@
-#ifndef ARG_HOOK_H
-#define ARG_HOOK_H
-
-#define ARG_HOOK_PATH_SIZE 255
-
-extern void arg_hook_main(unsigned int decoded_options_count, struct cl_decoded_option *decoded_options, int argc, char **argv);
-
-#endif
